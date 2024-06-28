14eb6f4ebfb1d88a35ca75fa5b0cbad050ab868e
Gustavo Henrique <145779028+hgrgustavo@users.noreply.github.com>
2024-06-28 18:32:45 -0300

    Delete databases directory

--- databases/emails/script.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE `service_email`;
-USE `service_email`;
-CREATE TABLE `Emails`(
-    `email_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `email_address` CHAR(255) NOT NULL,
-    `email_user` CHAR(255) NOT NULL,
-    `email_provider` CHAR(255) NOT NULL
-);
